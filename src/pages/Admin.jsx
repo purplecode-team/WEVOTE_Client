47bc505ee376935271413b1f4333fd8ed5fbf0ab
highcastle4242 <wnstjd4242@naver.com>
2021-02-21 01:15:40 +0900

    Feat: Page Routing 추가

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Admin = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Admin page</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default Admin;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Main = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Main page</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default Main;
