7540f483abc9262e6aadffb0ebf043d3f6d51583
highcastle4242 <wnstjd4242@naver.com>
2021-02-20 22:47:53 +0900

    Feat: paybooc을 기본 폰트로 추가

--- src/lib/styles/GlobalStyle.js
+++ src/lib/styles/GlobalStyle.js
@@ -2,7 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
+  ${reset};
+  html{
+    font-size: 10px;
+  }
+  body{
+    font-family: 'paybooc';
+  }
 `;
 
 export default GlobalStyle;
