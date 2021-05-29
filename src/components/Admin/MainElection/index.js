427bb04a2b00b200f2b5a8a23d715084b57f4f5e
highcastle4242 <wnstjd4242@naver.com>
2021-05-29 21:54:13 +0900

    Feat: Admin MainElection

---
+++ src/components/Admin/MainElection/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainEjectForm from './MainElectionForm';
+import MainEjectList from './MainElectionList';
+
+const MainEjectArticle = () => {
+  return (
+    <>
+      <MainEjectForm />
+      {/* <MainEjectList /> */}
+    </>
+  );
+};
+
+export default MainEjectArticle;
