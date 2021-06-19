8faf77e7f07223431222d91bae11b771ff874f47
highcastle4242 <wnstjd4242@naver.com>
2021-06-20 04:31:46 +0900

    Modify: Information api 설정

--- src/components/Admin/Information/index.js
+++ src/components/Admin/Information/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import InformationForm from './InformationForm';
-// import InformationList from './InformationList';
+import InformationList from './InformationList';
 
 const InformationArticle = () => {
   return (
     <>
       <InformationForm />
-      {/* <InformationList /> */}
+      <InformationList />
     </>
   );
 };
