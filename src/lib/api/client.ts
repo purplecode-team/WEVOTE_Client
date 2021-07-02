fb26e288f0ebdeac26dac1b01996062bf6ee17c7
highcastle4242 <wnstjd4242@naver.com>
2021-07-02 14:49:25 +0900

    Refactor: 디렉토리 변경

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -4,18 +4,18 @@ const client = axios.create();
 
 // 글로벌 설정 예시:
 
-client.defaults.baseURL = 'http://34.64.235.182';
+client.defaults.baseURL = 'http://34.64.235.182:8080';
 // client.defaults.baseURL =
 client.defaults.withCredentials = true;
 
 axios.interceptors.request.use(
-  request => {
-    let accessToken = localStorage.getItem('x-access-token');
+  (request) => {
+    const accessToken = localStorage.getItem('x-access-token');
     // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
     client.defaults.headers.common['x-access-token'] = `${accessToken}`;
     return request;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
--- src/lib/hooks/useFetch.ts
+++ src/lib/hooks/useFetch.ts
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import client from '../../api/client';
+import client from '../api/client';
 
 type fetchProps = {
   url: string;
 };
 
 const useFetch = (props: fetchProps) => {
-  const {url} = props;
+  const { url } = props;
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [error, setError] = useState('initial error');
 
   useEffect(() => {
-    console.log(url)
+    console.log(url);
     const fetchUsers = async () => {
       try {
-        const response = await client.get('http://34.64.235.182'+url);
+        const response = await client.get(`http://34.64.235.182${url}`);
         setData(response.data);
       } catch (e) {
         setError(e);
