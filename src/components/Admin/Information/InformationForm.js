c9387478fdc9debb3edd5275ad0ed4054cb5c2e1
highcastlee <highcastlee@naver.com>
2021-07-16 17:10:21 +0900

    Modify: 공용 image uploader 생성 및 적용

--- src/components/Admin/Information/InformationForm.js
+++ src/components/Admin/Information/InformationForm.js
@@ -1,72 +1,98 @@
 import Button from '@material-ui/core/Button';
+import client from '../../../lib/api/client';
 import Grid from '@material-ui/core/Grid';
-import ImageUploader from 'react-images-upload';
+import ImageUploader from '../Common/ImageUploader';
 import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
+import { useAlert } from 'react-alert';
 import { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 function InformationForm (props) {
-  const { classes } = props;
-  const [pictures, setPictures] = useState([]);
+  const { classes, setUrl, confirmDeletion } = props;
+  const [files, setFiles] = useState([]);
+  const [urls, setUrls] = useState(['']);
+  const alert = useAlert();
 
-  const onDrop = picture => {
-    setPictures(picture);
-    console.log(picture);
+  const processImage = e => {
+    const imageFile = Object.values(e.target.files);
+    setFiles(imageFile);
+    setUrls(
+      imageFile.reduce((acc, cur) => {
+        return [...acc, URL.createObjectURL(cur)];
+      }, [])
+    );
   };
 
-  const submitImg = () => {
+  const resetImg = () => {
+    setFiles([]);
+    setUrls([]);
+  };
+
+  const submitImg = e => {
+    e.preventDefault();
     const formData = new FormData();
-    formData.append('img', pictures);
+    files.map(file => {
+      formData.append('img', file);
+    });
     const config = {
       headers: {
         'content-type': 'multipart/form-data',
       },
     };
-    try {
-      client
-        .post('/api/v1/register-info', formData, config)
-        .then(response => {
-          if (response.status !== 200) {
-            alert('이미지 등록 실패');
-            return;
-          }
-          setFile(null);
-          alert('이미지가 등록되었습니다');
-        })
-        .catch(error => {
-          alert('이미지 등록 실패');
-        });
-    } catch (e) {
-      console.error(e);
-    }
+    client
+      .post('/api/v1/admin/info', formData, config)
+      .then(response => {
+        if (response.status !== 200) {
+          alert.error('이미지 등록 실패');
+          return;
+        }
+        resetImg();
+        alert.success('이미지가 등록되었습니다');
+      })
+      .catch(e => {
+        alert.error('이미지 등록 실패');
+      })
+      .then(() =>
+        // 등록 직후 fetch 실행 시, 새로운 db가 적용 안 되는 이슈 해결법
+        window.setTimeout(setUrl(new String('/api/v1/admin/info')), 2000)
+      );
   };
 
   return (
     <Paper className={classes.paper}>
       <div className={classes.contentWrapper}>
         <form onSubmit={submitImg}>
-          {/* <Calender fileUrl={fileUrl} /> */}
-          <ImageUploader
-            {...props}
-            className={classes.uploader}
-            withIcon
-            buttonText='Choose images'
-            onChange={onDrop}
-            imgExtension={['.jpg', '.gif', '.png', '.gif']}
-            maxFileSize={5242880}
-            withPreview
-          />
+          <Grid item xs={12} className={classes.buttonWrap}>
+            <ImageUploader
+              alt={'information'}
+              fileUrl={urls}
+              resetImg={resetImg}
+              processImage={processImage}
+              width={'250px'}
+              height={'auto'}
+              multiple
+            />
+          </Grid>
           <Grid item xs={12} className={classes.buttonWrap}>
             <Button
-              className={classes.submit}
+              className={classes.button}
+              variant='contained'
+              color='primary'
+              type='button'
+              onClick={() => confirmDeletion('all')}
+            >
+              {'전체 삭제'}
+            </Button>
+            <Button
+              className={classes.button}
               variant='contained'
               color='primary'
               type='submit'
             >
-              등록
+              {'등록'}
             </Button>
           </Grid>
         </form>
@@ -89,22 +115,16 @@ const styles = theme => ({
   },
   buttonWrap: {
     textAlign: 'right',
-    margin: '20px',
+    margin: '40px 20px',
+    height: '100%',
   },
-  submit: {
+  button: {
     width: '100px',
     height: '40px',
     borderRadius: '15px',
+    margin: '0 10px',
   },
 });
-
-const PreviewImg = styled.img`
-  height: 200px;
-`;
-
-const Input = styled.input`
-  margin: 20px auto;
-`;
 
 InformationForm.propTypes = {
   classes: PropTypes.object.isRequired,
